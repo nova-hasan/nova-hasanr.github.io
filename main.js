80984942b07e44d8ebd7f2729cde9ab9bf28b9aa
nova-hasan <117144806+nova-hasan@users.noreply.github.com>
2024-04-21 22:03:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function alert(pesan) {
 document.getElementById("bookSubmit");
 console.log("Berhasil Input judul buku");
 }
+//Contoh kode program
+function welcome() {
+    alert('Sim salabim muncullah elemen-elemen HTML!');
+    const contents = document.querySelector('.contents');
+    contents.removeAttribute('hidden');
+  }
+  document.body.onload = welcome;
+//Contoh kode program
+document.getElementById('incrementButton').onclick = increment;
+  document.body.onload = welcome;
